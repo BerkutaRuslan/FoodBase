0297e1dd4bdbfe66b35d08ebf4d694db2941c1b0
Ruslan <berkutruslan1@gmail.com>
2020-10-15 15:16:27 +0300

    updated project urls

--- FoodBase/urls.py
+++ FoodBase/urls.py
@@ -10,11 +10,3 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
                                                                                       document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    from django.conf.urls.static import static
-
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
