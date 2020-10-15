2fc2e17de406260cf71866b67a4ae6975d581690
Ruslan <berkutruslan1@gmail.com>
2020-10-15 14:51:26 +0300

    updated urls.py

--- FoodBase/urls.py
+++ FoodBase/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from FoodBase import settings
 
@@ -7,6 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                      document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
