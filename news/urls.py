a1a99a2f3cb90fbb45d997f898df841100cf023a
Ruslan <berkutruslan1@gmail.com>
2020-10-15 15:08:45 +0300

    added new news endpoints

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,6 @@ from news.views import *
 
 urlpatterns = [
     path('all_news', GetAllNewsView.as_view()),
+    path('<int:pk>', GetSingleNewsView.as_view()),
+    path('random', GetRandomNewsView.as_view()),
 ]
--- news/views.py
+++ news/views.py
@@ -1,15 +1,33 @@
+from django.http import HttpResponse
+import os
+from FoodBase.settings import HOST
 from rest_framework import status, generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from news.models import News
-from news.serializers import AllNewsSerializer
+from news.serializers import NewsSerializer
 
 
 class GetAllNewsView(generics.ListAPIView):
     permission_classes = [AllowAny]
     queryset = News.objects.all()
-    serializer_class = AllNewsSerializer
+    serializer_class = NewsSerializer
 
+
+class GetSingleNewsView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+class GetRandomNewsView(APIView):
+    def get(self, request):
+        random_news = News.objects.order_by('?').first()
+        return Response({"id": random_news.id,
+                         "title": random_news.title,
+                         "description": random_news.description,
+                         "creation_date": random_news.creation_date.isoformat(' ', 'seconds'),
+                         "photo": f'/media/{random_news.photo}'}, status=status.HTTP_200_OK)
 
