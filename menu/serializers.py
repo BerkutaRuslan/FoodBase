019d4514a856f77368576fc338dd91920ceba1b9
Ruslan <berkutruslan1@gmail.com>
2020-10-20 10:16:26 +0300

    created menu endpoints, also added filtering via query params

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from menu.models import Drink
+from menu.models import Drink, Dish, MenuOfDay
 
 
 class DrinkSerializer(serializers.ModelSerializer):
@@ -9,3 +9,18 @@ class DrinkSerializer(serializers.ModelSerializer):
         model = Drink
         fields = '__all__'
 
+
+class DishSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Dish
+        fields = '__all__'
+
+
+class MenuOfDaySerializer(serializers.ModelSerializer):
+    dish = DishSerializer(many=False, read_only=True)
+    drink = DrinkSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = MenuOfDay
+        fields = ['dish', 'drink', 'price', 'expire']
