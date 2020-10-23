8d91cfd5dda075126bf9dc34eca432e3305aeb07
Ruslan <berkutruslan1@gmail.com>
2020-10-23 13:45:55 +0300

    minor fixes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from accounts.models import User, ResetKey, Employee
+from accounts.models import User, Employee
 
 admin.site.register(User)
 admin.site.register(Employee)
