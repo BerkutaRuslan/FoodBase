0db158ee72da0342574f83c88aba69478d3ffc7a
Ruslan <berkutruslan1@gmail.com>
2020-10-23 13:14:41 +0300

    added User, Employee, reset key models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+from FoodBase.utils import generate_token
+from restaurant.models import Restaurant
+
+department_choices = (
+    ('security', 'security'),
+    ('cleaner', 'cleaner'),
+    ('cook', 'cook'),
+    ('manager', 'manager'),
+    ('main_head', 'main_head'),
+)
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=128, blank=True, null=True)
+    last_name = models.CharField(max_length=128, blank=True, null=True)
+    email = models.EmailField(max_length=254, null=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True, help_text="phone as username")
+    passcode = models.CharField(max_length=4, null=True, blank=True)
+    passcode_timer = models.DateTimeField(null=True)
+    photo = models.ImageField(upload_to='users')
+    address = models.CharField(max_length=250, blank=True)
+    balance = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.first_name) + ' ' + str(self.last_name)
+
+
+class ResetKey(models.Model):
+    user = models.ForeignKey(User, related_name='reset_key', on_delete=models.CASCADE)
+    reset_key = models.CharField(max_length=40, default=generate_token)
+
+
+class Employee(models.Model):
+    user = models.ForeignKey(User, related_name='employees', on_delete=models.CASCADE)
+    salary = models.FloatField()
+    department = models.CharField(max_length=100, choices=department_choices)
+    started_from = models.DateField(auto_now_add=True)
+    restaurant = models.ForeignKey(Restaurant, related_name='employees', on_delete=models.CASCADE)
