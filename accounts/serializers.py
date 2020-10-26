631954f747d52f450d2fd3e1b9063d9c5003f212
Ruslan <berkutruslan1@gmail.com>
2020-10-26 11:20:31 +0200

    changed fields in user full serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,7 +7,7 @@ from accounts.models import User
 class UserFullSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['first_name', 'last_name', 'email', 'phone', 'photo', 'address', 'balance']
 
 
 class UserPhoneSerializer(serializers.ModelSerializer):
