2f8e493ca34c7d9ae5207c6c7c09de1c4fcb988e
Ruslan <berkutruslan1@gmail.com>
2020-12-17 15:36:16 +0200

    changed quantity field in model cart to positive only

--- menu/models.py
+++ menu/models.py
@@ -83,7 +83,7 @@ class Product(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.product.name
